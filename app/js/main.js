298a582fe80350ecf76bddd6dc7ef9c7620757cf
Volkovvvv <vladvolkovvv@mail.ru>
2020-10-14 18:07:32 +0300

    added product-slider and product

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,15 @@
 $(function(){
-
+    $(".rate-star").rateYo({
+        rating:5,
+        starWidth: "12px",
+        readOnly:true
+      });
+     
+    var mixer = mixitup('.products__inner-box');
+    $('.product-slider__inner').slick({
+        dots:true,
+        arrows:false,
+        slidesToShow:4,
+        slidesToScroll: 4
+    });
 });
