fdbd4d383d5abc91fd44355aa765116d1efdb7f5
Volkovvvv <vladvolkovvv@mail.ru>
2020-11-28 20:56:20 +0300

    added new items page

--- app/js/main.js
+++ app/js/main.js
@@ -5,11 +5,35 @@ $(function(){
         readOnly:true
       });
      
-    var mixer = mixitup('.products__inner-box');
+   
     $('.product-slider__inner').slick({
         dots:true,
         arrows:false,
         slidesToShow:4,
         slidesToScroll: 4
     });
+
+     $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        grid: false,
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: "$"
+    });
+    $('.icon-th-list').on('click',function(){
+        $('.product__item').addClass('list');
+        $('.icon-th-list').toggleClass('active');
+        $('.icon-th-large').removeClass('active');
+    })
+    $('.icon-th-large').on('click',function(){
+        $('.product__item').removeClass('list');
+        $('.icon-th-large').addClass('active');
+        $('.icon-th-list').removeClass('active');
+
+    });
+
+    
+    var mixer = mixitup('.products__inner-box');
 });
